eb723552f7f5a0ae192f30897a936cab21ef2abe
DeXt3R <swatantradasmgm@gmail.com>
2021-07-05 21:39:55 +0530

    Commit 2

--- AndroidStudioProjects/MiwokApp/app/src/main/java/com/example/miwokapp/NumbersActivity.java
+++ AndroidStudioProjects/MiwokApp/app/src/main/java/com/example/miwokapp/NumbersActivity.java
@@ -3,23 +3,43 @@ package com.example.miwokapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class NumbersActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        String []eng= new String[10];
-        eng[0]="One";
-        eng[1]="Two";
-        eng[2]="Three";
-        eng[3]="Four";
-        eng[4]="Five";
-        eng[5]="Six";
-        eng[6]="Seven";
-        eng[7]="Eight";
-        eng[8]="Nine";
-        eng[9]="Ten";
+        ArrayList<String> num=new ArrayList<String>();
+        num.add("One");
+        num.add("Two");
+        num.add("Three");
+        num.add("Four");
+        num.add("Five");
+        num.add("Six");
+        num.add("Seven");
+        num.add("Eight");
+        num.add("Nine");
+        num.add("Ten");
+
+//        LinearLayout view=findViewById(R.id.rootView);
+//        for(int i=0;i<num.size();i++)
+//        {
+//            TextView numb=new TextView(this);
+//            numb.setText(num.get(i));
+//            view.addView(numb);
+//            numb.setTextSize(20)
+//        }
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, R.layout.num_layout, num);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+
     }
 }
